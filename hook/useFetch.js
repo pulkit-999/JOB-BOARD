299d5b285ae0646d54521b235ede2241ee7ebbe2
pulkit-999 <pulkitagrawal777@gmail.com>
2023-08-19 00:03:12 +0530

    commit

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+// import {RAPID_API_KEY} from "@env";
 
+
+// const rapidApiKey=RAPID_API_KEY
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -10,8 +13,8 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
-      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+      'X-RapidAPI-Key': '9a79631470mshe0807e0ed099873p1c88edjsn88a1ca366cc8',
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     },
     params: { ...query },
   };
