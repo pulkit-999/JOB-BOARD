3b39f5c443dbf1dd7648cc02765f1fc4a3878808
pulkit-999 <pulkitagrawal777@gmail.com>
2023-08-25 20:56:22 +0530

    commit

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,18 @@
 import { Stack,useRouter} from "expo-router";
 import { useFonts } from "expo-font";
+
 // import * as SplashScreen from "expo-splash-screen";
 
 // SplashScreen.preventAutoHideAsync();
-
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "home",
+};
 const Layout =()=>{
-    const [fontsLoaded] = useFonts({
-        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
-        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
-        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+   let[fontsLoaded]= useFonts({
+        'DMBold': require("../assets/fonts/DMSans-Bold.ttf"),
+        'DMMedium': require("../assets/fonts/DMSans-Medium.ttf"),
+        'DMRegular': require("../assets/fonts/DMSans-Regular.ttf"),
     });
 
     if (!fontsLoaded) {
